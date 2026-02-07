fd92aec67260c8d6ebfb0aeaad099860324c8595
Stonky-Boi <arnav63366@gmail.com>
2026-02-07 13:05:47 +0530

    basic cleanup

--- Inline-Completion-Extension/out/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cleanCompletion = cleanCompletion;
-function cleanCompletion(text) {
-    return text
-        .replace(/^```[\s\S]*?```/, "")
-        .trimEnd();
-}
-//# sourceMappingURL=utils.js.map
